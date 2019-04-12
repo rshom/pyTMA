cartesian works but covariance is wrong

runSim.py
@@ -8,13 +8,13 @@ import matplotlib.pyplot as plt
 
 from subcon import *
 
-runTime = 50.
+runTime = 200.
 dT = 1
 
 def main():
     # Generate ownship and target ship
-    ownship = Ship( np.array([ 0,0,2,2 ] ))
-    target = Ship( np.array([ 0,50,0,2 ] ))
+    ownship = Ship( np.array([ 0,0,0,2 ] ))
+    target = Ship( np.array([ -50,-50,1,1 ] ))
 
     # Take initial bearing to target and create contact
     contact = Contact( sonar_bearing( ownship, target ) )
@@ -30,8 +30,8 @@ def main():
         U = np.array([0,0,0,0])
         
         # move ships
-        if time == 20:
-            Xt, U = ownship(dT, newCourse=[-2,2])
+        if time == 30:
+            Xt, U = ownship(dT, newCourse=[2,-2])
         else:
             Xo, _ = ownship(dT)
 

subcon.py
@@ -41,6 +41,7 @@ class Contact:
         vRel = -speed*np.cos(bearing)
         
         self.xEst = np.array([ xRel, yRel, uRel, vRel])
+        self.xEst = np.array([ -50,-50,0,1 ] )
         self.pEst = np.eye(len(self.xEst))
         
 
@@ -55,7 +56,7 @@ class Contact:
 
         # Predict based on motion model and input from ship motion U
         xPred = motionModel@self.xEst - ownshipAcceleration
-        pPred = motionModel@self.pEst@motionModel.T+Q
+        pPred = motionModel@self.pEst@motionModel.T#+Q*.001
         bearingPred = np.arctan2(xPred[0],xPred[1])
 
         # Compare bearing prediction and observation
@@ -71,6 +72,7 @@ class Contact:
 
         ## Correction for observation
         S = jH@pPred@jH.T+R
+        print(S)
         G = pPred@jH.T@np.linalg.inv(S)
 
         ## final estimate